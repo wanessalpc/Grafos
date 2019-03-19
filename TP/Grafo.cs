129f80e54754fed4928697ba082d1496addd1667
Rafael Badaró <rafabadaro.15@gmail.com>
2019-03-19 00:52:37 -0300

    Método de isAdjaceten criado

--- TP/Grafo.cs
+++ TP/Grafo.cs
@@ -1,18 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TP
 {
     public class Grafo
     {
         List<List<Vertice>> grafo = new List<List<Vertice>>();
+        /*
+        grafo - [
+        
+        [v1, ...],
+        [v2, ...],
+        [v3, ...]
+        ]   
+        
+         Vértice inicial representa a sub lista do mesmo
+         v1  -> /
+         v2  -> v2 -> v3
+         v3  -> v1 -> v1 -> v2
+        */
 
         public bool isAdjacente(Vertice v1, Vertice v2)
         {
-
+            bool isAdj = false;
+            grafo.ForEach(subLista =>
+            {
+                if (subLista.Contains(v1) && subLista.Contains(v2))
+                {
+                    int indexV1 = subLista.IndexOf(v1);
+                    int indexV2 = subLista.IndexOf(v2);
+                    if (indexV1 - 1 == indexV2 || indexV1 + 1 == indexV2)
+                    {
+                        isAdj = true;
+                    }
+                }
+            });
+            return isAdj;
         }
         public int getGrau(Vertice v1)
         {
