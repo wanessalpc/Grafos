7110ea5cc6c94cf6ebcca2669ff10db9765c883f
Rafael Badaró <rafabadaro.15@gmail.com>
2019-03-18 23:57:08 -0300

    Criando os métodos

---
+++ TP/Grafo.cs
@@ -0,0 +1,65 @@
+﻿namespace TP
+{
+    public class Grafo
+    {
+
+
+        public bool isAdjacente(Vertice v1, Vertice v2)
+        {
+
+        }
+        public int getGrau(Vertice v1)
+        {
+
+        }
+        bool isIsolado(Vertice v1)
+        {
+
+        }
+        bool isPendente(Vertice v1)
+        {
+
+        }
+        bool isRegular()
+        {
+
+        }
+        bool isNulo()
+        {
+
+        }
+        bool isCompleto()
+        {
+
+        }
+        bool isConexo()
+        {
+
+        }
+        bool isEuleriano()
+        {
+
+        }
+        bool isUnicursal()
+        {
+
+        }
+        Grafo getComplementar()
+        {
+
+        }
+        Grafo getAGMPrim(Vertice v1)
+        {
+
+        }
+        Grafo getAGMKruskal()
+        {
+
+        }
+        int getCutVertices()
+        {
+
+        }
+
+    }
+}
