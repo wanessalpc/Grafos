42586335deb78c07ead441a0b9b72eb8628f4dbe
Rafael Badaró <rafabadaro.15@gmail.com>
2019-03-23 22:20:13 -0300

    Métodos de GetGrau e de imprimir o grafo

--- TP/Grafo.cs
+++ TP/Grafo.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 
 namespace TP
@@ -6,7 +6,7 @@ namespace TP
     public class Grafo
     {
 
-        Dictionary<Vertice, List<Vertice>> grafo = new Dictionary<Vertice, List<Vertice>>();
+        public Dictionary<Vertice, List<Vertice>> grafo = new Dictionary<Vertice, List<Vertice>>();
 
         /*      
             Key, Value -> no caso a nossa key é um vértice e o value é uma Lista de vértices
@@ -14,73 +14,116 @@ namespace TP
             [v1][v1 -> v2 -> v3]
             [v2][ListaDeAdj]
         */
-
+     
         public bool IsAdjacente(Vertice v1, Vertice v2)
         {
             bool isAdj = false;
-            List<Vertice> subLista;
-            if (grafo.TryGetValue(v1, out subLista)) // O TryGetValue retorna um booleano, se encontra o value ele é passado pra variável no out
+            List<Vertice> listaDeAdj;
+            if (grafo.TryGetValue(v1, out listaDeAdj)) // O TryGetValue retorna um booleano, se encontra o value ele é passado pra variável no out
             {
-                if (subLista.Contains(v2))
+                if (listaDeAdj.Contains(v2))
                 {
                     isAdj = true;
+                    Console.WriteLine("Os vértices : " + v1.valor + " e " + v2.valor + " são adjacentes");
+                }
+                else
+                {
+                    Console.WriteLine("Não são adjacentes :(");
                 }
             }
+            
 
             return isAdj;
 
         }
-        public int getGrau(Vertice v1)
+        public int GetGrau(Vertice v1)
         {
-
+            List<Vertice> listaDeAdj;
+            if(grafo.TryGetValue(v1, out listaDeAdj))
+            {
+                return listaDeAdj.Count; // para cada vértice na lista de adjacentes, o grau do vértice aumenta
+            }
+            else
+            {
+                return 0; // se não achar o valor ele não tem adjacente
+            }
         }
-        bool isIsolado(Vertice v1)
-        {
+        //bool IsIsolado(Vertice v1)
+        //{
 
-        }
-        bool isPendente(Vertice v1)
-        {
+        //}
+        //bool IsPendente(Vertice v1)
+        //{
 
-        }
-        bool isRegular()
-        {
+        //}
+        //bool IsRegular()
+        //{
 
-        }
-        bool isNulo()
-        {
+        //}
+        //bool IsNulo()
+        //{
 
-        }
-        bool isCompleto()
-        {
+        //}
+        //bool IsCompleto()
+        //{
 
-        }
-        bool isConexo()
-        {
+        //}
+        //bool IsConexo()
+        //{
 
-        }
-        bool isEuleriano()
-        {
+        //}
+        //bool IsEuleriano()
+        //{
 
-        }
-        bool isUnicursal()
-        {
+        //}
+        //bool IsUnicursal()
+        //{
 
-        }
-        Grafo getComplementar()
-        {
+        //}
+        //Grafo GetComplementar()
+        //{
 
-        }
-        Grafo getAGMPrim(Vertice v1)
-        {
+        //}
+        //Grafo GetAGMPrim(Vertice v1)
+        //{
 
-        }
-        Grafo getAGMKruskal()
-        {
+        //}
+        //Grafo GetAGMKruskal()
+        //{
 
-        }
-        int getCutVertices()
-        {
+        //}
+        //int GetCutVertices()
+        //{
 
+        //}
+
+        public void ImprimirGrafo(Dictionary<Vertice, List<Vertice>> grafo)
+        {
+            Console.WriteLine("Imprimindo os valores do grafo: ");
+
+            foreach (Vertice v in grafo.Keys)
+            {
+                List<Vertice> listaDeAdj;
+                Console.Write("Vértice " + v.valor + ":");
+                if (grafo.TryGetValue(v, out listaDeAdj))
+                {
+                    if (listaDeAdj.Count > 0)
+                    {
+                        listaDeAdj.ForEach(vertice => Console.Write(" -> " + vertice.valor));
+                    }
+                    else
+                    {
+                        Console.Write(" -> _ ");  // indica que é uma lista vazia
+                    }
+                }
+                else
+                {
+                    Console.Write(" -> _ ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
         }
 
     }
--- TP/Program.cs
+++ TP/Program.cs
@@ -8,8 +8,30 @@ namespace TP
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Grafo g = new Grafo();
+
+            Vertice v1 = new Vertice() , v2 = new Vertice() , v3 = new Vertice();
+            v1.valor = 1;
+            v2.valor = 2;
+            v3.valor = 3;
+            List<Vertice> adj1 = new List<Vertice>();
+            List<Vertice> adj2 = new List<Vertice>();
+            List<Vertice> adj3 = new List<Vertice>();
+
+            adj1.Add(v2);
+            adj1.Add(v3);
+            adj2.Add(v1);
+            adj2.Add(v3);
+            adj3.Add(v1);
+            adj3.Add(v2);
+
+            g.grafo.Add(v1, adj1);
+            g.grafo.Add(v2, adj2);
+            g.grafo.Add(v3, adj3);
+
+           
         }
     }
 }
